351f9251f5b628bb160409c03563e567735edb94
jason <whispersinthesoundofsilence2@gmail.com>
2018-08-13 19:34:34 +0100

    Initialising the tick component and pseudo-coding AimTowardsCrosshair()

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -16,7 +16,27 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair(); //TODO
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank)
+	{
+		return;
+	}
+	else {
+		//Get world location if linetrace through crosshair
+		//If it hits the landscape
+			//Tell controlled tank to aim this point
+	}
 }
